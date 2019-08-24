d411d03af01b6312cc0fc47d4cf1407d2047e194
zhangruiispangpang <54319255+zhangruishipangpang@users.noreply.github.com>
2019-08-24 21:50:13 +0800

    new file own

---
+++ static/js/myJS.js
@@ -0,0 +1,25 @@
+var color = ['white', 'pink', 'red', 'rgb(216, 90, 90)', 'yellow', 'green', 'blue', 'crimson', 'orange'];
+var i = 0;
+
+
+$(document).ready(function () {
+
+    sleep(0);
+})
+
+function sleep(count) {
+    setTimeout(function () {
+        changeColor();
+        if (count < 300) {
+            sleep(++count);
+        }
+    }, 800);
+
+}
+function changeColor() {
+    if (i == color.length) {
+        i = 0;
+    }
+    // alert(color[i]+'-'+i);
+    $('.changeColor').css('color', color[i++]);
+}
